9bb37d1b57148f07fdb77e5fc0dca9a67b7dfd0a
Enmanuel Almonte <ealmonterozon@gmail.com>
2016-07-20 12:10:46 -0400

    add main and signup pages to frontend

---
+++ static/js/blog.js
@@ -0,0 +1,15 @@
+/**
+ * Created by HMachine on 20/07/2016.
+ */
+$(document).ready(function(){
+    switch (window.location.pathname){
+        case "/":
+            $("a.active").removeClass("active");
+            $("a.home-nav-item").addClass("active");
+            break;
+        case "/signup":
+            $("a.active").removeClass("active");
+            $("a.signup-nav-item").addClass("active");
+            break;
+    }
+});
