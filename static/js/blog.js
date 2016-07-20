701a6e058d637f55de21629fc38565236d2b2ba4
Enmanuel Almonte <ealmonterozon@gmail.com>
2016-07-20 15:01:42 -0400

    add tables to admin page

--- static/js/blog.js
+++ static/js/blog.js
@@ -16,4 +16,41 @@ $(document).ready(function(){
             $("a.admin-nav-item").addClass("active");
             break;
     }
+
+    //Show more/ less blog post body
+    // Configure/customize these variables.
+    var showChar = 330;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "Show more >";
+    var lesstext = "Show less";
+
+
+    $('.more').each(function() {
+        var content = $(this).html();
+
+        if(content.length > showChar) {
+
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h +
+                '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+            $(this).html(html);
+        }
+
+    });
+
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
 });
