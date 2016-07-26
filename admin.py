d0d6adb350fd16d7813c6c02d20902fd7a8407f2
enmanuelar <ealmonterozon@gmail.com>
2016-07-26 12:10:48 -0800

    add top posts by comments and manage post section to admin page

--- admin.py
+++ admin.py
@@ -1,8 +1,24 @@
-import webapp2
+import webapp2, logging
 from main import *
+from itertools import groupby
+from collections import namedtuple, Counter
 
 class AdminHandler(Handler):
     @check_auth
     def get(self, **kwargs):
-        self.render("admin.html", user_logged=kwargs['user_logged'])
+        posts = blogdb.get_admin_entries()
+        post_id = blogdb.get_posts_id_by_comments()
+        post_id_list = []
+        l = []
+        d = namedtuple('top_post', ['key', 'title', 'user', 'comments_count'])
+        for post in post_id:
+            if post.post_id in post_id_list:
+                post_id_list.append((str(post.post_id)))
+            else:
+                post_id_list.append((str(post.post_id)))
+        for key, group in groupby(post_id_list):
+            entry = blogdb.get_single_entry(int(key))
+            l.append([d(key, entry.title, entry.user, len(list(group)))])
+        l.sort(key=lambda comment: comment[0].comments_count, reverse=True)
+        self.render("admin.html", posts=posts, top_posts=l[:5], user_logged=kwargs['user_logged'])
 
--- blogdb.py
+++ blogdb.py
@@ -17,17 +17,26 @@ class Comments(db.Model):
     post_id = db.IntegerProperty(required=True)
 
 class Users(db.Model):
-    username = db.StringProperty(required = True)
-    password = db.StringProperty(required = True)
-    email = db.EmailProperty(required = False)
+    username = db.StringProperty(required=True)
+    password = db.StringProperty(required=True)
+    email = db.EmailProperty(required=False)
 
 def get_entries(page):
     limit = 4
     offset = limit * page
     return db.GqlQuery("SELECT * FROM Entry WHERE enabled = TRUE ORDER BY created DESC LIMIT %d OFFSET %d" %(limit, offset))
 
+def get_admin_entries():
+    return db.GqlQuery("SELECT * FROM Entry ORDER BY created DESC LIMIT 10")
+
 def get_comments(post_id):
     return db.GqlQuery("SELECT * FROM Comments WHERE post_id = %d ORDER BY created DESC" % (post_id))
+
+def get_single_entry(post_id):
+    return Entry.get_by_id(post_id)
+
+def get_posts_id_by_comments():
+    return db.GqlQuery("SELECT * FROM Comments ORDER BY post_id DESC")
 
 def get_user(username):
     return  db.Query(Users).filter('username =', username).get()
